5e2f6c91094fcb4138ced5bbcad9adde891ccf82
Ram Ashish <aram97010@gmail.com>
2023-08-14 10:41:09 +0530

    added start command to run the script

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express();
 dotenv.config({path:"./config/.env"});
 const PORT = process.env.PORT || 3000;
 
-console.log(process.env.PORT);
 app.use(express.json());
 app.use('/', router);
 
